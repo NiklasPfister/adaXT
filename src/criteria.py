b9bde30a1696ba65cc5343f8cbee4ab7484c7fba
Simon-program <56270036+Simon-program@users.noreply.github.com>
2023-06-26 10:15:12 +0200

    prototype branch Simon

---
+++ src/criteria.py
@@ -0,0 +1,10 @@
+def Gini_index():
+    """
+    Calculates the gini coefficient of a node
+
+    Args:
+        N (Node): Node object
+
+    Returns:
+        returns a double values of the gini index of a node
+    """
---
+++ src/tree.py
@@ -0,0 +1,5 @@
+class Node:
+    def __init__(self, values) -> None:
+        self.charecteristics = [] # list of all values below the node
+        
+        self.val = sum(leafnode) # leafnode only
