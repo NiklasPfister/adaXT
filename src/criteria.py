16dadfe0e5c02724f0baf53b871db0ff06f2f26d
Simon-program <simon@3450.dk>
2023-06-27 09:43:19 +0200

    Comments and so on

--- src/criteria.py
+++ src/criteria.py
@@ -3,7 +3,7 @@ def Gini_index():
     Calculates the gini coefficient of a node
 
     Args:
-        N (Node): Node object
+        data points
 
     Returns:
         returns a double values of the gini index of a node
--- src/tree.py
+++ src/tree.py
@@ -1,5 +1,45 @@
 class Node:
+    """
+    Node structure
+    
+    Args:
+        start: int used for indexing data
+        end: int used for indexing data
+
+    """
     def __init__(self, values) -> None:
         self.charecteristics = [] # list of all values below the node
-        
+        self.impurity = pass 
         self.val = sum(leafnode) # leafnode only
+
+
+class Tree:
+    """
+    Tree object
+
+    Args:
+        features: all x values
+        outcomes: all outcome values
+    """
+    pass
+
+class TreeBuilder:
+    """
+    Tree builder class
+
+    Args:
+        data: With the last column as the outcome values (data[-1])
+    """
+    
+    def buildTree():
+        """
+        Builds the tree:
+        
+        Returns:
+            return Tree object
+        """
+        pass
+    pass
+
+
+data = ["features"]
