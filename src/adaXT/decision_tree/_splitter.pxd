257b3f3209487be0480c8c4f12b8cea3decf661e
Simon-program <simon@3450.dk>
2023-09-05 08:57:40 -0400

    spllitter work

--- src/adaXT/decision_tree/_splitter.pxd
+++ src/adaXT/decision_tree/_splitter.pxd
@@ -1,10 +1,24 @@
 cimport numpy as cnp
 cnp.import_array()
+
+ctypedef cnp.float64_t npFloat
+ctypedef cnp.int_t npInt
+
+cdef class test_obj:
+    cdef:
+        float crit
+        list[:, :] idx_split
+        float[2] imp
+        float split_val
+
 cdef class Splitter:
     cdef:
-        double[:, :] features
-        double[:, :] outcomes
+        double[:] features
+        double[:] outcomes
         int n_features
-        int[:, :] pre_sort
+        int[:] pre_sort
+        int[:] indices
 
-    cdef cnp.ndarray sort_feature(self, cnp.ndarray[cnp.int_t, ndim=1] indices, cnp.ndarray[cnp.float64_t, ndim=1] feature)
+    cdef cnp.ndarray sort_feature(self, cnp.ndarray[npInt], cnp.ndarray[npFloat])
+
+    cdef int test_split(self, test_obj, int[:], int[:], int)
--- src/adaXT/decision_tree/_splitter.pyx
+++ src/adaXT/decision_tree/_splitter.pyx
@@ -4,29 +4,11 @@ from ._func_wrapper import FuncWrapper
 cnp.import_array()
 
 
-ctypedef cnp.float64_t npFloat
-ctypedef cnp.int_t npInt
 cdef class Splitter:
     """
     Splitter class used to create splits of the data
     """
-    def __init__(self, cnp.ndarray[npFloat, ndim=2] X, cnp.ndarray[npFloat, ndim=1] Y, cnp.ndarray[npInt, ndim=2] presort, criterion: FuncWrapper):
-        """
-        Parameters
-        ----------
-        X : npt.NDArray
-            The input features of the dataset
-
-        Y : npt.NDArray
-            The outcomes of the dataset
-
-        criterion : Callable, optional
-            Criteria function for calculating information gain,
-            if None it uses the specified function in the start of splitter.py
-        
-        presort : ndarray 
-            A sorted index list of the features 
-        """
+    def __init__(self, cnp.ndarray[npFloat] X, cnp.ndarray[npFloat, ndim = 1] Y, cnp.ndarray[npInt, ndim=2] presort, criterion: FuncWrapper):
         self.features = X 
         self.outcomes = Y 
 
@@ -36,22 +18,46 @@ cdef class Splitter:
         # self.constant_features = np.empty(len(self.features)) #TODO: not yet implemented
 
     cdef cnp.ndarray sort_feature(self, cnp.ndarray[npInt, ndim=1] indices, cnp.ndarray[npFloat, ndim=1] feature):
-        """
-        Parameters
-        ----------
-        indices : List[int]
-            A list of the indices which are to be sorted over
-        
-        feature: np.ndarray
-            A 1 dimensional numpy array containing the feature values that are to be sorted over
-            
-        Returns 
-        -----------
-        np.ndarray
-            A 1 dimensional numpy array containing the indices sorted given the feature values
-        """
-        cdef cnp.ndarray[npFloat, ndim=1] sort_list
+        cdef npFloat[:] sort_list
         sort_list = feature[indices]
         sort_list = np.argsort(sort_list)
 
         return indices[sort_list]
+
+    cdef int test_split(self, test_obj test, int[:] left_indices, int[:] right_indices, int feature):
+        cdef double[2] imp
+        cdef int[:] indices 
+        cdef double[:] curr
+        cdef double crit, mean_thresh
+        cdef int n_total, n_curr
+        cdef double[:] x, y
+
+        features = np.array(self.features)
+        outcomes = np.array(self.outcomes)
+        func_wrap = self.criteria
+        if func_wrap.func:
+            criteria = func_wrap.func
+        else:
+            raise MemoryError
+        
+        indices = self.indices
+        idx_split = [left_indices, right_indices]
+
+        n_total = len(indices)
+        for i in range(2): # Left and right side
+            curr = idx_split[i]
+            n_curr = len(curr)
+            if n_curr == 0:
+                continue
+            x = features[curr]
+            y = outcomes[curr]
+            imp[i] = criteria(x, y) # Calculate the impurity of current child
+            crit += imp[i] * (n_curr / n_total) # Weight by the amount of datapoints in child
+
+        mean_thresh = np.mean([features[left_indices[-1], feature], features[right_indices[0], feature]])
+        
+        test.crit = crit
+        test.idx_split = idx_split
+        test.imp = imp
+        test.split_val = mean_thresh
+        return 1
