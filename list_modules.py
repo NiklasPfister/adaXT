0ab52dea4d998fd9e911ef87c93ae5cde45b4c84
Simon-program <simon@3450.dk>
2023-10-12 17:24:52 -0400

    removed listing of modules.
    Also removed the github action on my branch.

--- list_modules.py
+++
@@ -1,9 +0,0 @@
-import pkgutil
-
-# Replace 'your_package_name' with the actual package name you want to inspect
-package_name = 'adaXT'
-
-# Use pkgutil to iteratively list all modules within the package
-package = __import__(package_name)
-for importer, modname, ispkg in pkgutil.walk_packages(package.__path__):
-    print(modname)
