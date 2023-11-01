7e6f1d322843c533a593def5202b0ddc6c46ab59
WilliamHeuser <sdr366@alumni.ku.dk>
2023-11-01 07:38:09 +0100

    Pulled in changes to criteria

--- src/adaXT/decision_tree/splitter.pyx
+++ src/adaXT/decision_tree/splitter.pyx
@@ -1,9 +1,8 @@
 import numpy as np
-from numpy import float64 as DOUBLE
 cimport numpy as cnp
+from numpy import float64 as DOUBLE
 cnp.import_array()
-from .func_wrapper cimport FuncWrapper
-from libc.stdlib cimport malloc, free
+from .criteria cimport Criteria
 from numpy.math cimport INFINITY
 
 
@@ -11,8 +10,7 @@ cdef class Splitter:
     """
     Splitter class used to create splits of the data
     """
-
-    def __init__(self, double[:, ::1] X, double[:] Y, criteria: FuncWrapper):
+    def __init__(self, double[:, ::1] X, double[:] Y, criteria: Criteria):
         '''
         Class initializer 
         ----------------
@@ -34,25 +32,6 @@ cdef class Splitter:
         self.criteria = criteria
         self.pre_sort = None
         self.n_class = len(np.unique(Y))
-
-    cpdef void make_c_lists(self, int n_class):
-        '''
-        Function to allocate memory for the classes of the response
-        ----------------
-        Parameters
-            ----------
-            n_class: int
-                The number of classes 
-        '''
-        self.class_labels = <double *> malloc(sizeof(double) * self.n_class)
-        self.n_in_class = <int *> malloc(sizeof(int) * self.n_class)
-
-    cpdef void free_c_lists(self):
-        '''
-        Function to deallocate memory for the classes of the response
-        '''
-        free(self.class_labels)
-        free(self.n_in_class)
 
     def set_pre_sort(self, pre_sort):
         '''
@@ -84,59 +63,7 @@ cdef class Splitter:
         cdef:
             double[:] temp
         temp = feature.base[indices] 
-        return np.array(indices.base[np.argsort(temp)], dtype=np.int32)  
-
-    cdef (double, double, double, double) evaluate_split(self, int[:] left_indices, int[:] right_indices, int feature):
-        """
-        Function to evaluate how good a split is
-        ----------
-
-        Parameters
-        ----------
-        left_indices : memoryview of NDArray
-            Indices of the left side of the split
-        
-        right_indices : memoryview of NDArray
-            Indices of the right side of the split
-        
-        feature: int
-            The current feature we are working on
-            
-        Returns 
-        -----------
-        (double, double, double, double)
-            A quadruple containing the criteria value, the left impurity, the right impurity
-            and the mean threshold between the two closest datapoints of the current feature
-        """
-        cdef:
-            double mean_thresh
-            FuncWrapper criteria = self.criteria
-            double left_imp = 0.0
-            double right_imp = 0.0
-            double crit = 0.0
-            double* class_labels = self.class_labels
-            int* n_in_class = self.n_in_class
-            int n_response = left_indices.shape[0] # initial
-            double[:, ::1] features = self.features
-            double[:] response = self.response
-        
-        # Calculate criteria value on the left dataset
-        if n_response == 0:
-            left_imp = 0.0
-        else:
-            left_imp = criteria.func(features, response, left_indices, class_labels, n_in_class)
-            crit = left_imp * (n_response/self.n_indices)
-        # Calculate criteria value on the right dataset
-        n_response = right_indices.shape[0]
-        if n_response == 0:
-            right_imp = 0.0
-        else:
-            right_imp += criteria.func(features, response, right_indices, class_labels, n_in_class)
-        
-        crit += (right_imp) * (n_response/self.n_indices)
-        mean_thresh = (features[left_indices[-1], feature] + features[right_indices[0], feature]) / 2
-        
-        return (crit, left_imp, right_imp, mean_thresh)
+        return np.array(indices.base[np.argsort(temp)], dtype=np.int32)   
 
     # Gets split given the indices in the given node
     cpdef get_split(self, int[:] indices):
@@ -170,13 +97,6 @@ cdef class Splitter:
             int i, feature # variables for loop
             int[:] left_indices, right_indices
             cnp.ndarray[cnp.int32_t, ndim=1] sorted_index_list_feature
-        
-        # If the classes list is not null, we have a classification tree
-        # In that case allocate memory for lists
-        if self.class_labels != NULL:
-            self.free_c_lists()
-            classes = np.unique(self.response.base[indices])
-            self.make_c_lists(len(classes))
  
         features = self.features.base
         n_features = self.n_features
@@ -198,15 +118,13 @@ cdef class Splitter:
                 # Skip one iteration of the loop if the current threshold value is the same as the next in the feature list
                 if current_feature_values[sorted_index_list_feature[i]] == current_feature_values[sorted_index_list_feature[i + 1]]:
                     continue 
-                # Split the dataset
-                left_indices = sorted_index_list_feature[:i + 1]
-                right_indices = sorted_index_list_feature[i + 1:]
-                crit, left_imp, right_imp, threshold = self.evaluate_split(left_indices, right_indices, feature) # test the split
-
-                if crit < best_score:
+                # test the split
+                crit, left_imp, right_imp, threshold = self.criteria.evaluate_split(sorted_index_list_feature, i+1, feature) 
+        
+                if (best_score - crit) > 1e-15: #rounding error
                     # Save the best split
-                    # The index is given as the index of the first element of the right dataset 
-                    best_feature, best_threshold, best_score, best_imp = feature, threshold, crit, [left_imp, right_imp] 
-                    split = [left_indices, right_indices]
+                    best_feature, best_threshold, best_score, best_imp = feature, threshold, crit, [left_imp, right_imp] # The index is given as the index of the first element of the right dataset 
+                    split = [sorted_index_list_feature[:i+1], sorted_index_list_feature[i+1:]]
+                    
         # Return the best split
         return split, best_threshold, best_feature, best_score, best_imp
