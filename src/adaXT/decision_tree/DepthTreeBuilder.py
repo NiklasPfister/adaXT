75c1fbbb6b174bdaa09f60d692c3f3c41179b9c4
Niklas Andreas Pfister <np@math.ku.dk>
2023-12-07 06:10:35 +0000

    Automated autopep8 fixes

--- src/adaXT/decision_tree/DepthTreeBuilder.py
+++ src/adaXT/decision_tree/DepthTreeBuilder.py
@@ -203,12 +203,16 @@ class DepthTreeBuilder:
                 # statements
                 N_t_L = len(split[0])
                 N_t_R = len(split[1])
-                is_leaf = (
-                    n_samples / n_obs * (impurity - (N_t_L / n_samples) * child_imp[0] - (N_t_R / n_samples) * child_imp[1]) < min_improvement + EPSILON or
-                    N_t_L < min_samples_leaf or
-                    N_t_R < min_samples_leaf or
-                    is_leaf
-                )
+                is_leaf = (n_samples /
+                           n_obs *
+                           (impurity -
+                            (N_t_L /
+                             n_samples) *
+                            child_imp[0] -
+                            (N_t_R /
+                             n_samples) *
+                            child_imp[1]) < min_improvement +
+                           EPSILON or N_t_L < min_samples_leaf or N_t_R < min_samples_leaf or is_leaf)
 
             if not is_leaf:
                 # Add the decision node to the List of nodes
