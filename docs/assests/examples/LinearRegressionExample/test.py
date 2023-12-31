95f1188c2033d83c4918ccb346f9c4282a2f27a9
Niklas Andreas Pfister <np@math.ku.dk>
2023-12-31 11:20:51 +0000

    Automated autopep8 fixes

--- docs/assests/examples/LinearRegressionExample/test.py
+++ docs/assests/examples/LinearRegressionExample/test.py
@@ -1,9 +1,10 @@
+import numpy as np
+import testCrit
 from adaXT.decision_tree import DecisionTree
 from adaXT.decision_tree.tree_utils import plot_tree
 import matplotlib.pyplot as plt
-import pyximport; pyximport.install()
-import testCrit
-import numpy as np
+import pyximport
+pyximport.install()
 
 n = 100
 m = 4
