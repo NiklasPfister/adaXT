4909557cd01936fdb819ea2e67f604a899dd70ba
Simon Vinding Brodersen <simon@3450.dk>
2024-01-05 14:40:33 +0100

    update to squared error documentation

--- src/adaXT/decision_tree/criteria.pyi
+++ src/adaXT/decision_tree/criteria.pyi
@@ -8,7 +8,7 @@ class Gini_index(Criteria):
     r"""
     Calculates the gini index given:
     $$
-    Gini Index = 1 - \sum_{i=1}^n (P[i])^2
+    \text{Gini Index} = 1 - \sum_{i=1}^n (P[i])^2
     $$
     Where $P_i$ denotes the probability of an element
     being classified for a distinct class.
@@ -19,7 +19,7 @@ class Entropy(Criteria):
     r"""
     Calculates the Entropy given:
     $$
-    E = - \sum_{i = 1}^n P[i] \log_2 (P[i])
+    \text{Entropy} = - \sum_{i = 1}^n P[i] \log_2 (P[i])
     $$
     Where $P[i]$ denotes the probability of randomly selecting an example
     in class i.
@@ -28,7 +28,16 @@ class Entropy(Criteria):
 
 class Squared_error(Criteria):
     r"""
-    (y[i] - u)^2/n_obs
+    Calculates the Squared error given by:
+    $$
+    \text{Squared Error} = \frac{\sum_{i = 1}^n (y[i] - \mu_y)^2}{n_{obs}}
+    $$
+    Where y denotes the outcome values in a node. $\mu_y$ denotes the mean value in the node,
+    and $n_{obs}$ denotes the number of observations in the node.
+
+    For a faster calculation it is calculated as:
+    $$
+    \text{Squared Error} = \frac{\sum_{i = 1}^n y[i]^2}{n_obs} - \mu_y^2
     """
     pass
 
@@ -36,7 +45,7 @@ class Linear_regression(Criteria):
     r"""
     Calculates the impurity of a Node by:
     $$
-    L = \sum_{i \in indices} (Y[i] - \\theta_0 - \\theta_1 X[i, 0])^2
+    \text{Linear Regression} = \sum_{i \in indices} (Y[i] - \\theta_0 - \\theta_1 X[i, 0])^2
     $$
     """
     pass
