0117f9a938c6b00e624c0b75251a50ff0741424f
Simon Vinding Brodersen <simon@3450.dk>
2024-09-29 15:13:31 +0200

    Fixed linting

--- src/adaXT/predict/predict.pyx
+++ src/adaXT/predict/predict.pyx
@@ -5,7 +5,6 @@ from ..decision_tree.nodes import DecisionNode
 from collections.abc import Sequence
 from statistics import mode
 cimport numpy as cnp
-from ..parallel import shared_numpy_array
 from ..parallel import ParallelModel
 
 # Use with cdef code instead of the imported DOUBLE
