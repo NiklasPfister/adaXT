9cd34b224dfdae0f17f15b45694d3c4a28440888
Simon Vinding Brodersen <simon@3450.dk>
2025-05-26 20:20:58 +0200

    Formatting random forest

--- src/adaXT/random_forest/random_forest.py
+++ src/adaXT/random_forest/random_forest.py
@@ -64,8 +64,7 @@ def get_sample_indices(
             resample_size0 = sampling_args["size"]
             resample_size1 = sampling_args["size"]
         else:
-            resample_size0 = np.min(
-                [sampling_args["split"], sampling_args["size"]])
+            resample_size0 = np.min([sampling_args["split"], sampling_args["size"]])
             resample_size1 = np.min(
                 [X_n_rows - sampling_args["split"], sampling_args["size"]]
             )
@@ -75,7 +74,7 @@ def get_sample_indices(
             replace=sampling_args["replace"],
         )
         pred_indices = gen.choice(
-            indices[sampling_args["split"]:],
+            indices[sampling_args["split"] :],
             size=resample_size1,
             replace=sampling_args["replace"],
         )
@@ -86,8 +85,7 @@ def get_sample_indices(
             resample_size0 = sampling_args["size"]
             resample_size1 = sampling_args["size"]
         else:
-            resample_size0 = np.min(
-                [sampling_args["split"], sampling_args["size"]])
+            resample_size0 = np.min([sampling_args["split"], sampling_args["size"]])
             resample_size1 = np.min(
                 [X_n_rows - sampling_args["split"], sampling_args["size"]]
             )
@@ -97,7 +95,7 @@ def get_sample_indices(
             replace=sampling_args["replace"],
         )
         pred_indices = gen.choice(
-            indices[sampling_args["split"]:],
+            indices[sampling_args["split"] :],
             size=resample_size1,
             replace=sampling_args["replace"],
         )
@@ -153,18 +151,11 @@ def build_single_tree(
         predictor=predictor,
         splitter=splitter,
     )
-    tree.fit(
-        X=X,
-        Y=Y,
-        sample_indices=fitting_indices,
-        sample_weight=sample_weight)
+    tree.fit(X=X, Y=Y, sample_indices=fitting_indices, sample_weight=sample_weight)
     if honest_tree:
         tree.refit_leaf_nodes(
-            X=X,
-            Y=Y,
-            sample_weight=sample_weight,
-            sample_indices=prediction_indices)
-
+            X=X, Y=Y, sample_weight=sample_weight, sample_indices=prediction_indices
+        )
     return tree
 
 
@@ -349,8 +340,7 @@ class RandomForest(BaseModel):
             if "size" not in sampling_args:
                 sampling_args["size"] = self.X_n_rows
             elif isinstance(sampling_args["size"], float):
-                sampling_args["size"] = int(
-                    sampling_args["size"] * self.X_n_rows)
+                sampling_args["size"] = int(sampling_args["size"] * self.X_n_rows)
             elif not isinstance(sampling_args["size"], int):
                 raise ValueError(
                     "The provided sampling_args['size'] is not an integer or float as required."
@@ -421,7 +411,8 @@ class RandomForest(BaseModel):
             sampling=self.sampling,
         )
         self.fitting_indices, self.prediction_indices, self.out_of_bag_indices = zip(
-            *indices)
+            *indices
+        )
         self.trees = self.parallel.starmap(
             build_single_tree,
             map_input=zip(self.fitting_indices, self.prediction_indices),
@@ -444,8 +435,9 @@ class RandomForest(BaseModel):
             n_jobs=self.n_jobs_fit,
         )
 
-    def fit(self, X: ArrayLike, Y: ArrayLike,
-            sample_weight: ArrayLike | None = None) -> None:
+    def fit(
+        self, X: ArrayLike, Y: ArrayLike, sample_weight: ArrayLike | None = None
+    ) -> None:
         """
         Fit the random forest with training data (X, Y).
 
@@ -477,8 +469,7 @@ class RandomForest(BaseModel):
         self.X = shared_numpy_array(X)
         self.Y = shared_numpy_array(Y)
         self.X_n_rows, self.n_features = self.X.shape
-        self.max_features = self._check_max_features(
-            self.max_features, X.shape[0])
+        self.max_features = self._check_max_features(self.max_features, X.shape[0])
         self.sample_weight = self._check_sample_weight(sample_weight)
         self.sampling_args = self.__get_sampling_parameter(self.sampling_args)
 
